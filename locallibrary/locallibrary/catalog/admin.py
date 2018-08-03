8719902f67321aacafdb447e51544b8e80730d93
Panda <mingkaityler@gmail.com>
2018-08-03 08:09:27 +0800

    Advance_Configuration(DjangoAdminSite)

--- locallibrary/locallibrary/catalog/admin.py
+++ locallibrary/locallibrary/catalog/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from .models import Author, Book, BookInstance, Genre
 
 # Register your models here.
+
+# admin.site.register(Author);
+"""Define Admin Class"""
+class AuthorAdmin(admin.ModelAdmin):
+    # pass;
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death');
+
+"""Register the admin class with the associated model"""
+admin.site.register(Author, AuthorAdmin)
+
+# admin.site.register(Book);
+#Register the Admin classes for Books Using decorator
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    # pass;
+    list_display = ('title', 'author', 'display_genre');
+
+# admin.site.register(BookInstance);
+#Register the Admin classes for BookInstance Using decorator
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_filter = ('status', 'due_back');
+    # pass;
+
+admin.site.register(Genre);
--- locallibrary/locallibrary/catalog/models.py
+++ locallibrary/locallibrary/catalog/models.py
@@ -8,6 +8,9 @@ class Genre(models.Model):
     """Model representing a book genre."""
     name = models.CharField(max_length = 200, help_text = 'Enter a book genre (e.g. Science Fiction)');
 
+    class Meta:
+        ordering = ['name'];
+
     def __str__(self):
         """String for representing the Model object."""
         return self.name;
@@ -15,7 +18,7 @@ class Genre(models.Model):
 class Book(models.Model):
     """Model representing a book (but not a specific copy of book)."""
     title = models.CharField(max_length = 200);
-    authur = models.ForeignKey('Author', on_delete = models.SET_NULL, null = True);
+    author = models.ForeignKey('Author', on_delete = models.SET_NULL, null = True);
 
     #Foreign Key used because book can only have one author, but authors can have multiple books.
     #Author as a string rather than object because it hasn't been declared yet in the file.
@@ -25,6 +28,11 @@ class Book(models.Model):
     #ManyToManyField used because genre can contain many books. Books can cover many genre.
     #Genre has already been defined so we can specify the object above.
     genre = models.ManyToManyField(Genre, help_text = 'Select a genre for this book.');
+
+    def display_genre(self):
+        """Create a string for the Genre. This is required to display genre in Admin."""
+        return ', '.join(genre.name for genre in self.genre.all()[:3])
+    display_genre.short_description = 'Genre';
 
     def __str__(self):
         """String representing the Model object"""
